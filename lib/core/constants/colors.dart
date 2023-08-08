805dbb22dba4802822ef1f8a6078192fe98cfcc5
Elias Kekana <thapeloelias.kekana@gmail.com>
2023-08-08 13:18:09 +0200

    Complete Donut Details Page and add two colors in colors.dart

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -4,6 +4,9 @@ class AppColors {
   const AppColors._();
   static const mainColor = Color(0xFFFF0F7E);
   static const mainDarkColor = Color(0xFF980346);
+  static const mainlightDarkColor = Color(0xFFe7c9d8);
   static const secondaryColor = Color(0xFFd4bbff);
   static const secondaryDarkColor = Color(0xFF7e6f99);
+
+  static const outcastColor = Color(0xFFf78dbd);
 }
--- lib/presentation/pages/donut_details_page.dart
+++ lib/presentation/pages/donut_details_page.dart
@@ -11,8 +11,8 @@ class DonutDetailsPage extends StatefulWidget {
 }
 
 class _DonutDetailsPageState extends State<DonutDetailsPage> {
-  final DonutModel selectedDonut = DonutModel.donuts.first;
-  
+  final DonutModel selectedDonut = DonutModel.donuts[5];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,10 +24,91 @@ class _DonutDetailsPageState extends State<DonutDetailsPage> {
         title: Image.asset(Utils.donutLogoRedText, width: 120),
       ),
       body: Column(children: [
-        Container(),
+        Container(
+          height: MediaQuery.sizeOf(context).height * 0.4,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Positioned(
+                top: -40,
+                right: -120,
+                child: Image.asset(
+                  selectedDonut.imgSrc,
+                  width: MediaQuery.sizeOf(context).width * 1.1,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ],
+          ),
+        ),
         Expanded(
-          child: Column(
-            children: [],
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        selectedDonut.name,
+                        softWrap: true,
+                        style: TextStyle(
+                          color: AppColors.mainDarkColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 50),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.favorite_outline_rounded,
+                        size: 30,
+                        color: AppColors.mainDarkColor,
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: AppColors.mainDarkColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Text(
+                    "R${selectedDonut.price.toStringAsFixed(2)}",
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(selectedDonut.description),
+                const SizedBox(height: 20),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 20),
+                      elevation: 0,
+                      fixedSize:
+                          Size.fromWidth(MediaQuery.sizeOf(context).width),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50)),
+                      backgroundColor: AppColors.mainlightDarkColor),
+                  icon: const Icon(
+                    Icons.shopping_cart,
+                    color: AppColors.mainDarkColor,
+                  ),
+                  label: const Text(
+                    "Add To Card",
+                    style: TextStyle(color: AppColors.mainDarkColor),
+                  ),
+                )
+              ],
+            ),
           ),
         )
       ]),
